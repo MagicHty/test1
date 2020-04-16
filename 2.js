14b575702dafc7a21ad4fb1954a8d5134d85901e
MagicHty <yhj-wuyou@163.com>
2020-04-16 21:47:16 +0800

    第二次提交

---
+++ 2.js
@@ -0,0 +1,18 @@
+var a=10;
+var b=20;
+var c=a;
+a=b;
+b=c;
+console.log(a,b);
+
+var e=10;
+var f=20;
+e=e+f;
+f=e-f
+e=e-f;
+console.log(e,f);
+
+var g=10;
+var h=20;
+[g,h]=[h,g]
+console.log(g,h);
